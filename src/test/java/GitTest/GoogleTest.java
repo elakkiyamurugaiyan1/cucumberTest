0a121736187e640210cfa02ae32409b41a126c18
elakkiyamurugaiyan1 <elakkiyamurugaiyan1>
2018-10-11 11:07:57 +0530

    First commit

---
+++ src/test/java/GitTest/GoogleTest.java
@@ -0,0 +1,15 @@
+package GitTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+    @Test
+    public void sampletest()
+    {
+        WebDriver wd = new FirefoxDriver();
+        wd.manage().window().maximize();
+        wd.get("https://www.google.co.in/");
+    }
+
+}
